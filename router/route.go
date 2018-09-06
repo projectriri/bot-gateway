8d75bdd4ff5f269a532d5d2122b51c2fcf838bd4
Riko Sakurauchi <lijiahao99131@gmail.com>
2018-09-06 11:31:06 +0800

    :sparkles: insufficient router

--- router/route.go
+++ router/route.go
@@ -1,6 +1,52 @@
 package router
 
+import (
+	. "github.com/projectriri/bot-gateway/types"
+	"regexp"
+	"strings"
+)
+
 func route() {
-	ch := make(chan bool)
-	<-ch
+PacketLoop:
+	for {
+		pkt := <-producerBuffer
+		for _, cc := range consumerChannelPool {
+
+			from := strings.ToLower(pkt.Head.From)
+			to := strings.ToLower(pkt.Head.To)
+
+			var formats []Format
+			for _, ac := range cc.Accept {
+				f, _ := regexp.MatchString(ac.From, from)
+				t, _ := regexp.MatchString(ac.To, to)
+				if f && t {
+					formats = ac.Formats
+					break
+				}
+			}
+
+			if formats == nil {
+				continue
+			}
+
+			for _, format := range formats {
+
+				if strings.ToLower(pkt.Head.Format.API) == strings.ToLower(format.API) &&
+					strings.ToLower(pkt.Head.Format.Method) == strings.ToLower(format.Method) &&
+					strings.ToLower(pkt.Head.Format.Protocol) == strings.ToLower(format.Protocol) {
+					*cc.Buffer <- pkt
+					continue PacketLoop
+				}
+
+				for _, cvt := range converters {
+					if cvt.IsConvertible(pkt.Head.Format, format) {
+						try := cvt.Convert(pkt, format, *cc.Buffer)
+						if try {
+							continue PacketLoop
+						}
+					}
+				}
+			}
+		}
+	}
 }
