75b01cf08f488ae9b8ccfeecbea22cad905059c5
Riko Sakurauchi <lijiahao99131@gmail.com>
2018-09-10 23:38:49 +0800

    :bug: fix router

--- router/route.go
+++ router/route.go
@@ -25,77 +25,87 @@ func processPacket(pkt *Packet) {
 	processedMap := make(map[string][]Packet)
 	processingMap := make(map[string][]chan []Packet)
 	mux := sync.Mutex{}
-	for _, cc := range consumerChannelPool {
-		log.Debugf("[router] pkt: %v TRYING cc: %+v", pkt.Head.UUID, cc)
-		var formats []Format
-		for _, ac := range cc.Accept {
-			f := ac.FromRegexp.MatchString(pkt.Head.From)
-			t := ac.ToRegexp.MatchString(pkt.Head.To)
-			if f && t {
-				formats = ac.Formats
-				break
+	for _, consumerChannel := range consumerChannelPool {
+		go func(cc *ConsumerChannel) {
+			log.Debugf("[router] pkt: %v TRYING cc: %+v", pkt.Head.UUID, cc)
+			var formats []Format
+			for _, ac := range cc.Accept {
+				f := ac.FromRegexp.MatchString(pkt.Head.From)
+				t := ac.ToRegexp.MatchString(pkt.Head.To)
+				if f && t {
+					formats = ac.Formats
+					break
+				}
 			}
-		}
 
-		if formats == nil || len(formats) == 0 {
-			continue
-		}
+			if formats == nil || len(formats) == 0 {
+				return
+			}
 
-		log.Debugf("[router] pkt: %v ACCEPTED BY cc: %+v", pkt.Head.UUID, cc)
+			log.Debugf("[router] pkt: %v ACCEPTED BY cc: %+v", pkt.Head.UUID, cc)
 
-		for _, format := range formats {
-			f := format.String()
+			for _, format := range formats {
+				f := format.String()
 
-			mux.Lock()
-			if convertedPacket, ok := processedMap[f]; ok {
-				if convertedPacket == nil {
+				mux.Lock()
+				if convertedPacket, ok := processedMap[f]; ok {
+					if convertedPacket == nil {
+						mux.Unlock()
+						continue
+					}
+					pushPacket(cc, convertedPacket)
 					mux.Unlock()
+					return
+				}
+
+				if pkt.Head.Format.String() == f {
+					processedMap[f] = []Packet{*pkt}
+					pushPacket(cc, processedMap[f])
+					mux.Unlock()
+					return
+				}
+
+				if converting, ok := processingMap[f]; ok && converting != nil {
+					ch := make(chan []Packet)
+					converting = append(converting, ch)
+					mux.Unlock()
+					result := <-ch
+					if result != nil {
+						pushPacket(cc, result)
+						return
+					}
 					continue
 				}
-				pushPacket(cc, convertedPacket)
-				mux.Unlock()
-				continue
-			}
 
-			if pkt.Head.Format.String() == f {
-				processedMap[format.String()] = []Packet{*pkt}
-				pushPacket(cc, processedMap[f])
-				mux.Unlock()
-				continue
-			}
-
-			if converting, ok := processingMap[f]; ok && converting != nil {
-				ch := make(chan []Packet)
-				go func(consumerChan *ConsumerChannel) {
-					result := <-ch
-					pushPacket(consumerChan, result)
-				}(cc)
-				converting = append(converting, ch)
-			} else {
 				processingMap[f] = make([]chan []Packet, 0)
+				mux.Unlock()
 				for _, cvt := range converters {
 					if cvt.IsConvertible(pkt.Head.Format, format) {
-						go func(consumerChan *ConsumerChannel) {
-							ok, result := cvt.Convert(*pkt, format)
-							if ok && result != nil {
-								pushPacket(consumerChan, result)
-
-								mux.Lock()
-								for _, ch := range processingMap[f] {
-									ch <- result
-									close(ch)
-								}
-								delete(processingMap, f)
-								processedMap[f] = result
-								mux.Unlock()
-								return
+						ok, result := cvt.Convert(*pkt, format)
+						if ok && result != nil {
+							pushPacket(cc, result)
+							mux.Lock()
+							for _, ch := range processingMap[f] {
+								ch <- result
+								close(ch)
 							}
-						}(cc)
+							delete(processingMap, f)
+							processedMap[f] = result
+							mux.Unlock()
+							return
+						}
 					}
 				}
+				mux.Lock()
+				for _, ch := range processingMap[f] {
+					close(ch)
+				}
+				delete(processingMap, f)
+				processedMap[f] = nil
+				mux.Unlock()
+				continue
 			}
-			mux.Unlock()
-		}
+		}(consumerChannel)
 	}
 }
 
