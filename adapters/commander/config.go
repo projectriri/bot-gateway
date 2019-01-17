d23d038861f469f5acfbd4d1a154b7dc17348474
LI JIAHAO <lijiahao99131@gmail.com>
2019-01-17 12:08:20 +0800

    :recycle: extract core plugin

--- adapters/commander/config.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type Config struct {
-	ChannelUUID   string   `toml:"channel_uuid"`
-	CommandPrefix []string `toml:"command_prefix"`
-	ResponseMode  uint8    `toml:"response_mode"`
-}
